cd5e531f6a8a91cff2e31e149cde372fad6581b2
ineoaas <justinleomaas@gmail.com>
2026-05-18 12:50:30 +0200

    Added CourseRepository to handle database operations

---
+++ Planademic.DAL/Repositories/ICourseRepository.cs
@@ -0,0 +1,12 @@
+using Planademic.Domain;
+
+namespace Planademic.DAL.Repositories;
+
+public interface ICourseRepository
+{
+    Task AddAsync(Course course);
+    Task<Course?> GetByJoinCodeAsync(string joinCode);
+    Task<List<Course>> GetByUserIdAsync(int userId);
+    Task AddEnrollmentAsync(CourseEnrollment enrollment);
+    Task<bool> IsEnrolledAsync(int courseId, int studentId);
+}
