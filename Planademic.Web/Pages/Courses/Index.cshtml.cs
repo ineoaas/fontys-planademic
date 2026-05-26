fb0ff87dc5736608f6ff5fa8b94baa6a63d166f6
ineoaas <justinleomaas@gmail.com>
2026-05-26 20:36:21 +0200

    Added course management functionality with join, create, and delete actions

--- Planademic.Web/Pages/Courses/Index.cshtml.cs
+++ Planademic.Web/Pages/Courses/Index.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Planademic.Web.Pages.Courses
 {
-    // TODO: Inject ICourseService in Iteration 2 to load real enrolled courses.
     public class IndexModel : PageModel
     {
         public void OnGet() { }
