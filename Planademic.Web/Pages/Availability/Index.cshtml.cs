d3476a89e56797c8ddf0cc58dc797a5173faf88d
ineoaas <justinleomaas@gmail.com>
2026-05-27 19:29:56 +0200

    Implemented availability grid page with save handler and register services

--- Planademic.Web/Pages/Availability/Index.cshtml.cs
+++ Planademic.Web/Pages/Availability/Index.cshtml.cs
@@ -1,10 +1,42 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Planademic.BLL.Services;
 
 namespace Planademic.Web.Pages.Availability
 {
-    // TODO: Inject IAvailabilityService in Iteration 2 to persist slot selections.
+    [Authorize(Roles = "Student")]
     public class IndexModel : PageModel
     {
-        public void OnGet() { }
+        private readonly IAvailabilityService _availabilityService;
+
+        public HashSet<(int Day, int Slot)> SavedSlots { get; private set; } = [];
+
+        public IndexModel(IAvailabilityService availabilityService)
+        {
+            _availabilityService = availabilityService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var studentId = GetStudentId();
+            var slots = await _availabilityService.GetSlotsAsync(studentId);
+            SavedSlots = AvailabilityService.ToGridSet(slots);
+        }
+
+        public async Task<IActionResult> OnPostSaveAsync([FromBody] List<SlotDto> slots)
+        {
+            var studentId = GetStudentId();
+            var selections = slots.Select(s => (s.Day, s.Slot)).ToList();
+            var domainSlots = AvailabilityService.BuildSlots(studentId, selections);
+            await _availabilityService.SaveSlotsAsync(studentId, domainSlots);
+            return new OkResult();
+        }
+
+        private int GetStudentId()
+            => int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        public record SlotDto(int Day, int Slot);
     }
 }
